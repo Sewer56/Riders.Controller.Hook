e38dcedfee40652daccbca8c210eb2331249b53c
Sewer <seweratwork@gmail.com>
2020-06-11 23:55:23 +0100

    Each and every time I get closer to doing things more "safely"

--- Riders.Controller.Hook/Riders.Controller.Hook/FourPlayerPatch.cs
+++ Riders.Controller.Hook/Riders.Controller.Hook/FourPlayerPatch.cs
@@ -2,8 +2,12 @@
 using System.Runtime.InteropServices;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Hooks.Definitions.X86;
+using Sewer56.SonicRiders.API;
 using Sewer56.SonicRiders.Functions;
 using Sewer56.SonicRiders.Structures.Enums;
+using Sewer56.SonicRiders.Structures.Tasks;
+using Sewer56.SonicRiders.Structures.Tasks.Base;
+using Sewer56.SonicRiders.Structures.Tasks.Enums.States;
 using Sewer56.SonicRiders.Utility;
 
 namespace Riders.Controller.Hook
@@ -12,50 +16,57 @@ namespace Riders.Controller.Hook
     {
         private IReloadedHooks _hooks;
         private IReloadedHooksUtilities _hooksUtilities;
-        private TaskTracker _taskTracker;
-        private IHook<Functions.DefaultTaskFnWithReturn> _hook;
+
+        private IHook<Functions.DefaultTaskFnWithReturn> _titleHook;
+        private IHook<Functions.DefaultTaskFnWithReturn> _charaSelectHook;
+        private RaceMode _mode;
 
         public FourPlayerPatch(IReloadedHooks hooks, IReloadedHooksUtilities hooksUtilities)
         {
             _hooks = hooks;
             _hooksUtilities = hooksUtilities;
-            _taskTracker = new TaskTracker();
-            _hook = Functions.CharaSelectTask.Hook(CharacterSelectImpl).Activate();
+            _titleHook = Functions.TitleSequenceTask.Hook(TitleSequenceImpl).Activate();
+            _charaSelectHook = Functions.CharaSelectTask.Hook(CharacterSelectImpl).Activate();
         }
 
-        private unsafe int CharacterSelectImpl()
+        private unsafe byte TitleSequenceImpl()
         {
-            if (_taskTracker.CharacterSelect != null && _taskTracker.TitleSequence != null)
+            var task = (Task<TitleSequence, TitleSequenceTaskState>*)(*State.CurrentTask);
+            _mode = task->TaskData->RaceMode;
+            return _titleHook.OriginalFunction();
+        }
+
+        private unsafe byte CharacterSelectImpl()
+        {
+            var task = (Task<CharacterSelect, CharacterSelectTaskState>*)(*State.CurrentTask);
+            var charaData = task->TaskData;
+
+            switch (_mode)
             {
-                var titleData = _taskTracker.TitleSequence->TaskData;
-                var charaData = _taskTracker.CharacterSelect->TaskData;
-                switch (titleData->RaceMode)
-                {
-                    case RaceMode.TimeTrial:
-                        charaData->MaximumPlayerCount = 1;
-                        break;
-                    case RaceMode.GrandPrix:
-                        charaData->MaximumPlayerCount = 2;
-                        break;
-                    default:
-                        charaData->MaximumPlayerCount = 4;
-                        break;
-                }
+                case RaceMode.TimeTrial:
+                    charaData->MaximumPlayerCount = 1;
+                    break;
+                case RaceMode.GrandPrix:
+                    charaData->MaximumPlayerCount = 2;
+                    break;
+                default:
+                    charaData->MaximumPlayerCount = 4;
+                    break;
             }
 
-            return _hook.OriginalFunction();
+            return _charaSelectHook.OriginalFunction();
         }
 
         public void Resume()
         {
-            _hook.Enable();
-            _taskTracker.Enable();
+            _charaSelectHook.Enable();
+            _titleHook.Enable();
         }
 
         public void Suspend()
         {
-            _hook.Disable();
-            _taskTracker.Disable();
+            _charaSelectHook.Disable();
+            _titleHook.Disable();
         }
     }
 }
